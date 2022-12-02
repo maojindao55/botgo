7e21beec13295d7ca80240fe9e024a7c43c063cc
maojindao55 <hobby0524@gmail.com>
2022-12-02 22:37:31 +0800

    fix templateid type

--- dto/message_create.go
+++ dto/message_create.go
@@ -27,7 +27,7 @@ type Markdown struct {
 	//TemplateID       string            `json:"template_id"`        // 模版 id
 	CustomTemplateID string            `json:"custom_template_id"` // 自定义模版 id
 	Params           []*MarkdownParams `json:"params"`             // 模版参数
-	Content          string            `json:"content"`            // 原生 markdown
+	//Content          string            `json:"content"`            // 原生 markdown
 }
 
 // MarkdownParams markdown 模版参数 键值对
